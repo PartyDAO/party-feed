d3577e843f1f1f37bd055ae9fb9440303f259065
Steve Klebanoff <steve.klebanoff@gmail.com>
2021-12-20 12:34:47 -0800

    move up

--- scripts/docker_compose_dev.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-DOTENV_PATH=/code/.env.local docker-compose -f docker-compose.base.yml -f docker-compose.dev.yml "$@"
--- scripts/remote_logs.sh
+++
@@ -1,3 +0,0 @@
-# /bin/sh
-# run after evaling the response of docker-compose env
-./scripts/docker_compose_remote.sh logs -t -f --tail=500
