d2a8b9f4cd7faaa022f06bb9ae4bc50c02139168
Marcus Molchany <marcusmolchany@gmail.com>
2022-01-30 16:50:36 -0800

    add utils file

---
+++ src/utils.ts
@@ -0,0 +1,10 @@
+import { ethersProvider } from "./ethereum";
+
+export const bestUserName = async (address: string): Promise<string> => {
+  const ens = await ethersProvider.lookupAddress(address);
+  if (ens) {
+    return ens;
+  } else {
+    return address;
+  }
+};
