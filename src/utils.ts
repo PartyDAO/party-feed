e8d29be4c8724ca593c2b185c3e7bc3253e84137
Marcus Molchany <marcusmolchany@gmail.com>
2022-02-02 15:14:27 -0800

    add utils for determining party states

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import { PartyInfo } from "types";
 import { ethersProvider } from "./ethereum";
 
 export const bestUserName = async (address: string): Promise<string> => {
@@ -7,4 +8,22 @@ export const bestUserName = async (address: string): Promise<string> => {
   } else {
     return address;
   }
+};
+
+export const getIsNewPartyWithContribution = (party: PartyInfo): boolean => {
+  const { createdBy } = party;
+
+  // get all contributions
+  // filter out contributions by creator
+  // if length of resulting list is 0 OR > 1, return false
+  // if length of resulting list is 1, true
+  return false;
+};
+
+export const getIsPartyHalfWay = (party: PartyInfo): boolean => {
+  // get all contributions
+  // add up all contributions until amount raised >= (amountNeeded / 2)
+  // if the contributions list after this contribution is length 0, return true
+  // otherwise, return false
+  return false;
 };
