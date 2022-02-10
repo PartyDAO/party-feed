fb2dc3575c5bdf35edd99cbd18848685d1b402f8
Marcus Molchany <marcusmolchany@gmail.com>
2022-02-10 15:31:29 -0800

    add total contributed to party to Contribution type

--- src/types.ts
+++ src/types.ts
@@ -15,6 +15,7 @@ export interface Bid {
 export interface Contribution {
   contributorAddress: string;
   amountInEth: string;
+  totalAmountContributedToParyInWei: string;
 }
 
 export interface Finalization {
