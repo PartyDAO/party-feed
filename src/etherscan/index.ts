0a95736b55a3b02ade7f72874fdf31ba529405be
Marcus Molchany <marcusmolchany@gmail.com>
2022-04-15 14:58:02 -0700

    fix typo in comment

--- src/etherscan/index.ts
+++ src/etherscan/index.ts
@@ -9,7 +9,7 @@ const etherscanTasks = async (event: PartyEvent) => {
   }
 
   // do not attempt to verify the proxy contract on the start party event. this is to
-  // prevent a race condition where the we attempt to verify the contract source and
+  // prevent a race condition where we attempt to verify the contract source and
   // proxy contract source at the same time. the party contract must be verified before
   // the proxy contract can be verified. it takes a considerable amount of time to
   // verify the party contract, therefore verifying the party contract and proxy contract
